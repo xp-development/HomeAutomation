e63c21d4239120dea300b14cda60edf80a330b6e
Jan Schubert <dev@schubert-jan.de>
2018-12-26 20:59:32 +0100

    Protocols.App: Add GetAllRoomsResponseDataParser.

---
+++ Source/HomeAutomation.Protocols.App.UnitTests/_v0/_ResponseParsers/_Rooms/_GetAllRoomsResponseDataParser/Parse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using HomeAutomation.Protocols.App.v0.ResponseParsers;
+using HomeAutomation.Protocols.App.v0.ResponseParsers.Rooms;
+using Xunit;
+
+namespace HomeAutomation.Protocols.App.UnitTests._v0._ResponseParsers._Rooms._GetAllRoomsResponseDataParser
+{
+  public class Parse
+  {
+    [Fact]
+    public void ShouldSetRoomIdentifierArrayWithOneRoom()
+    {
+      var parser = new GetAllRoomsResponseDataParser();
+
+      var response = parser.Parse(0x00, 0x0001, 0x00, 0x00, new byte[] { 0x0A, 0x00, 0x00, 0x00 });
+
+      response.RoomIdentifiers.Length.Should().Be(1);
+      response.RoomIdentifiers[0].Should().Be(BitConverter.ToInt32(new byte[] { 0x0A, 0x00, 0x00, 0x00 }, 0));
+    }
+
+    [Fact]
+    public void ShouldSetRoomIdentifierArrayWithTwoRooms()
+    {
+      var parser = new GetAllRoomsResponseDataParser();
+
+      var response = parser.Parse(0x00, 0x0001, 0x00, 0x00, new byte[] { 0x0A, 0x00, 0x00, 0x00, 0x0B, 0x00, 0x00, 0x00 });
+
+      response.RoomIdentifiers.Length.Should().Be(2);
+      response.RoomIdentifiers[0].Should().Be(BitConverter.ToInt32(new byte[] { 0x0A, 0x00, 0x00, 0x00 }, 0));
+      response.RoomIdentifiers[1].Should().Be(BitConverter.ToInt32(new byte[] { 0x0B, 0x00, 0x00, 0x00 }, 0));
+    }
+
+    [Fact]
+    public void ShouldSetRoomIdentifierArrayWithoutRooms()
+    {
+      var parser = new GetAllRoomsResponseDataParser();
+
+      var response = parser.Parse(0x00, 0x0001, 0x00, 0x00, new byte[0]);
+
+      response.RoomIdentifiers.Length.Should().Be(0);
+    }
+  }
+}
---
+++ Source/HomeAutomation.Protocols.App/v0/ResponseParsers/Rooms/GetAllRoomsResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeAutomation.Protocols.App.v0.ResponseParsers.Rooms
+{
+  public class GetAllRoomsResponse : IResponse
+  {
+    public byte ProtocolVersion { get; }
+    public byte RequestType0 => 0x02;
+    public byte RequestType1 => 0x01;
+    public byte RequestType2 => 0x00;
+    public byte RequestType3 => 0x00;
+    public ushort Counter { get; }
+    public byte ResponseCode0 { get; }
+    public byte ResponseCode1 { get; }
+    public int[] RoomIdentifiers { get; }
+
+    public GetAllRoomsResponse(byte protocolVersion, ushort counter, byte responseCode0, byte responseCode1, int[] roomIdentifiers)
+    {
+      ProtocolVersion = protocolVersion;
+      Counter = counter;
+      ResponseCode0 = responseCode0;
+      ResponseCode1 = responseCode1;
+      RoomIdentifiers = roomIdentifiers;
+    }
+  }
+}
