3085009e170d0825a57434f43d9c30c83d890d3a
Jan Schubert <dev@schubert-jan.de>
2018-12-28 20:38:00 +0100

    Protocols.App: Add GetRoomDescriptionResponseDataParser.

---
+++ Source/HomeAutomation.Protocols.App.UnitTests/_v0/_ResponseParsers/_Rooms/_GetRoomDescriptionResponseDataParser/Parse.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using HomeAutomation.Protocols.App.v0.ResponseParsers.Rooms;
+using Xunit;
+
+namespace HomeAutomation.Protocols.App.UnitTests._v0._ResponseParsers._Rooms._GetRoomDescriptionResponseDataParser
+{
+  public class Parse
+  {
+    [Fact]
+    public void ShouldGetRoomDescription()
+    {
+      var parser = new GetRoomDescriptionResponseDataParser();
+
+      var response = parser.Parse(0x00, 0x0001, 0x00, 0x00, new byte[] { 0x74, 0x00, 0x65, 0x00, 0x73, 0x00, 0x74, 0x00 });
+
+      response.Description.Should().Be("test");
+    }
+
+    [Fact]
+    public void RoomDescriptionShouldBeEmptyIfResponseCodeIsUnknownRoomIdentifier()
+    {
+      var parser = new GetRoomDescriptionResponseDataParser();
+
+      var response = parser.Parse(0x00, 0x0001, 0x01, 0x00, new byte[] { 0x74, 0x00, 0x65, 0x00, 0x73, 0x00, 0x74, 0x00 });
+
+      response.Description.Should().BeEmpty();
+    }
+  }
+}
---
+++ Source/HomeAutomation.Protocols.App/v0/ResponseParsers/Rooms/GetRoomDescriptionResponseDataParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeAutomation.Protocols.App.v0.ResponseParsers.Rooms
+{
+  public class GetRoomDescriptionResponseDataParser : IResponseDataParser
+  {
+    public GetRoomDescriptionResponse Parse(byte protocolVersion, ushort counter, byte responseCode0, byte responseCode1, IEnumerable<byte> data)
+    {
+      var description = "";
+      if (responseCode0 == 0x00 && responseCode1 == 0x00)
+      {
+        description = Encoding.Unicode.GetString(data.ToArray());
+      }
+
+      return new GetRoomDescriptionResponse(protocolVersion, counter, responseCode0, responseCode1, description);
+    }
+
+    IResponse IResponseDataParser.Parse(byte protocolVersion, ushort counter, byte responseCode0, byte responseCode1, IEnumerable<byte> data)
+    {
+      return Parse(protocolVersion, counter, responseCode0, responseCode1, data);
+    }
+  }
+}
