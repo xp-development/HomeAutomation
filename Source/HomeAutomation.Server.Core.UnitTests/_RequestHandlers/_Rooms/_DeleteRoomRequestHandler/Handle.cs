8c4229eb38249daac2b871c3ca2fbe1ac958e057
Jan Schubert <dev@schubert-jan.de>
2019-11-20 21:38:11 +0100

    Add request handler for delete room.

---
+++ Source/HomeAutomation.Server.Core.UnitTests/_RequestHandlers/_Rooms/_DeleteRoomRequestHandler/Handle.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using HomeAutomation.Protocols.App.v0.Requests.Rooms;
+using HomeAutomation.Protocols.App.v0.Responses.Rooms;
+using HomeAutomation.Server.Core.DataAccessLayer;
+using HomeAutomation.Server.Core.RequestHandlers.Rooms;
+using Xunit;
+
+namespace HomeAutomation.Server.Core.UnitTests._RequestHandlers._Rooms._DeleteRoomRequestHandler
+{
+  public class Handle : DatabaseTestBase
+  {
+    [Fact]
+    public void ShouldDeleteRoomDForExistingEntry()
+    {
+      using (var context = new ServerDatabaseContext())
+      {
+        context.Rooms.Add(new Room {Id = 1, Description = "Room 1"});
+        context.Rooms.Add(new Room {Id = 2, Description = "Room 2"});
+        context.Rooms.Add(new Room {Id = 300, Description = "Room 300"});
+        context.SaveChanges();
+      }
+      var requestHandler = new DeleteRoomRequestHandler();
+
+      var response = (DeleteRoomDataResponse) requestHandler.Handle(new DeleteRoomDataRequest { RoomIdentifier = 2 });
+
+      response.RoomIdentifier.Should().Be(2);
+      response.ResponseCode0.Should().Be(0);
+      response.ResponseCode1.Should().Be(0);
+    }
+
+    [Fact]
+    public void ShouldReturnErrorResponseCodeIfIdIsUnknown()
+    {
+      using (var context = new ServerDatabaseContext())
+      {
+        context.Rooms.Add(new Room {Id = 1, Description = "Room 1"});
+        context.Rooms.Add(new Room {Id = 2, Description = "Room 2"});
+        context.Rooms.Add(new Room {Id = 300, Description = "Room 300"});
+        context.SaveChanges();
+      }
+      var requestHandler = new DeleteRoomRequestHandler();
+
+      var response = (DeleteRoomDataResponse) requestHandler.Handle(new DeleteRoomDataRequest { RoomIdentifier = 99 });
+
+      response.ResponseCode0.Should().Be(0x01);
+      response.ResponseCode1.Should().Be(0x00);
+      response.RoomIdentifier.Should().Be(0);
+    }
+  }
+}
---
+++ Source/HomeAutomation.Server.Core/RequestHandlers/Rooms/DeleteRoomRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using HomeAutomation.Protocols.App.v0;
+using HomeAutomation.Protocols.App.v0.Requests;
+using HomeAutomation.Protocols.App.v0.Requests.Rooms;
+using HomeAutomation.Protocols.App.v0.Responses;
+using HomeAutomation.Protocols.App.v0.Responses.Rooms;
+using HomeAutomation.Server.Core.DataAccessLayer;
+
+namespace HomeAutomation.Server.Core.RequestHandlers.Rooms
+{
+  [RequestType(2, 4, 0, 0)]
+  public class DeleteRoomRequestHandler : IRequestHandler
+  {
+    public IResponse Handle(IRequest request)
+    {
+      var dataRequest = (DeleteRoomDataRequest) request;
+
+      using (var context = new ServerDatabaseContext())
+      {
+        var room = context.Rooms.Find(dataRequest.RoomIdentifier);
+        return room == null
+          ? new DeleteRoomDataResponse {RoomIdentifier = 0, ResponseCode0 = 0x01, ResponseCode1 = 0x00}
+          : new DeleteRoomDataResponse {RoomIdentifier = room.Id};
+      }
+    }
+  }
+}
